ea6d4b7f99366992c285104d130d2ec8dd07110f
Joshua Wise <joshua@joshuawise.com>
2025-01-16 04:52:33 -0500

    reduce overall BOM on panels, load manufacturer and MPN in

--- module-panels/mkpanelbom.py
+++ module-panels/mkpanelbom.py
@@ -27,8 +27,13 @@ for fp in pcb.footprints:
         fpks[key] = []
     fpks[key].append(fp)
 
+total_placements = 0
+
 print('"Id";"Designator";"Footprint";"Quantity";"Value";"Manufacturer";"MPN"')
 for id,k in enumerate(fpks):
     fps = fpks[k]
+    total_placements += len(fps)
     alldes = ",".join(fp.reference for fp in fps)
     print(f"{id+1};\"{alldes}\";\"{fps[0].fp_name}\";{len(fps)};\"{fps[0].value}\";\"{fps[0].manufacturer}\";\"{fps[0].mpn}\"")
+
+print(f"{len(fpks)} unique BOM items, with {total_placements} total placements", file=sys.stderr)
